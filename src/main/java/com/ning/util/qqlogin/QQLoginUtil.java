d7f7ac22e878cefda8ba71a0f3d770912bffc10a
jyxk <jyxking007@gmail.com>
2019-10-31 22:08:47 +0800

    add util include qqlogin

---
+++ src/main/java/com/ning/util/qqlogin/QQLoginUtil.java
@@ -0,0 +1,32 @@
+package com.ning.util.qqlogin;
+
+import com.ning.exception.login.LoginException;
+import com.qq.connect.QQConnectException;
+import com.qq.connect.api.OpenID;
+import com.qq.connect.javabeans.AccessToken;
+import com.qq.connect.oauth.Oauth;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * QQ登陆工具类
+ *
+ * @author wangn
+ * @date 2017/5/31
+ */
+public class QQLoginUtil {
+    public static String getUserOpenID(HttpServletRequest request) throws LoginException {
+        String openID;
+        try {
+            AccessToken accessTokenObj = (new Oauth()).getAccessTokenByRequest(request);
+            if ("".equals(accessTokenObj.getAccessToken())) {
+                throw new LoginException("未获取到参数！");
+            } else {
+                openID = new OpenID(accessTokenObj.getAccessToken()).getUserOpenID();
+            }
+        } catch (QQConnectException e) {
+            throw new LoginException(e.getMessage());
+        }
+        return openID;
+    }
+}
